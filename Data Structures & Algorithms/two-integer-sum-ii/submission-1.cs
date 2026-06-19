98e64b9d017a75db7243b74d9b152661c51c28ed
Salvatore Sammarco <microjmpr@gmail.com>
2026-06-19 11:25:33 +0200

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int i = 0;
+        int j = numbers.Length - 1;
+
+        while (i < j)
+        {
+            if (numbers[i] + numbers[j] == target)
+            {
+                return new int[] { i + 1, j + 1 };
+            }
+
+            if (numbers[i] + numbers[j] > target)
+            {
+                j--;
+            }
+
+            if (numbers[i] + numbers[j] < target)
+            {
+                i++;
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
